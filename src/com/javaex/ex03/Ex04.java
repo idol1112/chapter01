c30995aadb37301cc506b88aaa848b4377daed86
idol1112 <idol981112@gmail.com>
2021-05-27 10:10:53 +0900

    chapter01

--- src/com/javaex/ex03/Ex04.java
+++ src/com/javaex/ex03/Ex04.java
@@ -9,11 +9,14 @@ public class Ex04 {
 		System.out.print("근무시간:");
 		int money = 10000;
 		int time = sc.nextInt();
+		int sum = 0;
 		if(time<=8) {
-			System.out.println("받으실 임금은 "+time*money+"원 입니다.");
+			sum = time*money;
+			System.out.println("받으실 임금은 "+sum+"원 입니다.");
 		}
 		else {
-			System.out.println("받으실 임금은 "+((time-8)*12000+(money*8))+"원 입니다.");
+			sum = (time-8)*12000+(money*8);
+			System.out.println("받으실 임금은 "+sum+"원 입니다.");
 		}
 		
 		sc.close();
